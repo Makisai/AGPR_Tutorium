a1a7afa60d27f587c3b0544de419dd5629c207b8
Makisai <jschmaglowski@aol.com>
2018-04-18 08:34:02 +0200

    small changes + error&views pages

--- Übung1_11.4.2018/server.js
+++ Übung1_11.4.2018/server.js
@@ -11,3 +11,28 @@ const port = 3000;
 app.listen(port, function(){
   console.log('listening on port' + port);
 });
+
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/index.html');
+});
+
+app.post('/calculate', function(request, response){
+  const bitrate = request.body['bitrate'];
+  const duration = request.body['duration'];
+
+  if (isNaN(bitrate) || isNaN(duration) || bitrate < 0 || duration < 0){
+    console.log('ERROR: Eingabe überpüfen');
+    response.render('error',{
+    'bitrate': bitrate,
+    'duration': duration,
+    'message': 'Please check your Entry :)'
+  });
+  }else{
+    console.log('bitrate: '+ bitrate + ' duration: ' + duration);
+    response.render('result',{
+      'bitrate': bitrate,
+      'duration': duration,
+      'result': bitrate * duration
+    })
+  }
+});
